1adedbab96f669d7538f2b0bfb7dd7bc53bba8f0
jdp_ <42700985+jdpatdiscord@users.noreply.github.com>
2023-06-03 15:49:59 -0400

    tests now actually work

--- tests/testrunner.cpp
+++ tests/testrunner.cpp
@@ -45,8 +45,8 @@ int main(int argc, char* argv[])
 	for (auto& entry : std::filesystem::directory_iterator("./testbin"))
 	{
 		std::filesystem::path entrypath = std::filesystem::absolute(entry.path());
-		
-		testprocess(entrypath.c_str());
+		int res = testprocess(entrypath.c_str());
+		printf("Code %i for %s\n", res, entry.path().filename().string().c_str());
 	}
 	return 0;
 }
